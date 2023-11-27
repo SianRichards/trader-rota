ff7e5dc10f2cc6b2979b4def26af9ee3d2fd5e92
SianRichards <sian_richards@hotmail.co.uk>
2023-11-27 16:12:03 +0000

    display dummy data, & retrieve real data

--- trader-rota/src/components/Calendar/index.tsx
+++ trader-rota/src/components/Calendar/index.tsx
@@ -1,29 +1,53 @@
-import React from "react";
-
-const startDate = new Date("2023-01-01");
-const endDate = new Date("2023-12-31");
-
-const getAllDaysOfTheYear = () => {
-  const daysOfTheYear = [];
-
-  let currentDate = new Date(startDate);
-
-  while (currentDate <= endDate) {
-    daysOfTheYear.push(new Date(currentDate).toLocaleDateString("en-UK"));
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
-  return daysOfTheYear;
-};
-
-console.log(getAllDaysOfTheYear());
+import React, { useEffect, useState } from "react";
+import { dummyShiftData } from "../../data/dummyData";
+import { getTraderRotaInfo } from "../../api";
 
 const Calendar = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getTraderRotaInfo().then((response) => {
+      setData(response);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Calendar</h1>
-      {getAllDaysOfTheYear().map((date, index) => {
-        return <div key={index}>{date}</div>;
-      })}
+      <table>
+        {dummyShiftData.map((shiftEntry, index) => {
+          return (
+            <tr key={index}>
+              <td>{shiftEntry.date}</td>
+              <td>{shiftEntry.day}</td>
+              <td>
+                AM:
+                {shiftEntry.AM.map((AMShift, index) => {
+                  return (
+                    <div key={index}>
+                      <p>Desk 1: {AMShift.desk_1.name}</p>
+                      <p>Desk 2: {AMShift.desk_2.name}</p>
+                      <p>Desk 3: {AMShift.desk_3.name}</p>
+                    </div>
+                  );
+                })}
+              </td>
+              <td>
+                PM:
+                {shiftEntry.PM.map((AMShift, index) => {
+                  return (
+                    <div key={index}>
+                      <p>Desk 1: {AMShift.desk_1.name}</p>
+                      <p>Desk 2: {AMShift.desk_2.name}</p>
+                      <p>Desk 3: {AMShift.desk_3.name}</p>
+                    </div>
+                  );
+                })}
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 };
