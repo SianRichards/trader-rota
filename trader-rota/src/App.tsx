d5f9dd5c78eef585ad0c9949e5039106c1d93ac2
SianRichards <sian_richards@hotmail.co.uk>
2023-11-29 17:28:44 +0000

    index and app pages

---
+++ trader-rota/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Calendar from "./components/Calendar";
+import "./tailwind.css";
+
+const App = () => {
+
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
