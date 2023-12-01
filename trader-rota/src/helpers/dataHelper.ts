6ec24d5ad1c2bebce33ac724ee56cde399f4b2b5
SianRichards <sian_richards@hotmail.co.uk>
2023-12-01 16:01:07 +0000

    specify types

--- trader-rota/src/helpers/dataHelper.ts
+++ trader-rota/src/helpers/dataHelper.ts
@@ -1,7 +1,9 @@
-export const extractTraderNames = (data: any) => {
+import { IRotaPlan, IShift, IShiftData } from "../types";
+
+export const extractTraderNames = (data: IRotaPlan) => {
   let nameArray: string[] = [];
   data.forEach((dataObject: any) => {
-    let desks: any = ["desk_1", "desk_2", "desk_3"];
+    let desks: Array<string> = ["desk_1", "desk_2", "desk_3"];
     desks.forEach((desk: string) => {
       if (!nameArray.includes(dataObject[desk]) && dataObject[desk] !== "") {
         nameArray.push(dataObject[desk]);
@@ -11,10 +13,10 @@ export const extractTraderNames = (data: any) => {
   return nameArray;
 };
 
-export const addShiftsToTrader = (traders: any, data: any) => {
-  return traders.map((trader: any) => {
-    let shifts: any = [];
-    data.forEach((dataObj: any) => {
+export const addShiftsToTrader = (traders: Array<string>, data: IRotaPlan) => {
+  return traders.map((trader: string) => {
+    let shifts: Array<IShift> = [];
+    data.forEach((dataObj: IShiftData) => {
       const desks = [dataObj.desk_1, dataObj.desk_2, dataObj.desk_3];
       if (desks.includes(trader)) {
         shifts.push({
@@ -30,9 +32,9 @@ export const addShiftsToTrader = (traders: any, data: any) => {
   });
 };
 
-export const getArrayOfDates = (data: any) => {
-  let datesArray: any[] = [];
-  data.map((shift: any) => {
+export const getArrayOfDates = (data: IRotaPlan) => {
+  let datesArray: string[] = [];
+  data.map((shift: IShiftData) => {
     if (!datesArray.includes(shift.date)) {
       datesArray.push(shift.date);
     }
@@ -40,14 +42,14 @@ export const getArrayOfDates = (data: any) => {
   return datesArray;
 };
 
-export const getStartDate = (arrayOfDates: any) => {
+export const getStartDate = (arrayOfDates: Array<string>) => {
   const orderedDates = arrayOfDates.sort((a: string, b: string) => {
     return Date.parse(a) - Date.parse(b);
   });
   return orderedDates[0];
 };
 
-export const getEndDate = (arrayOfDates: any) => {
+export const getEndDate = (arrayOfDates: Array<string>) => {
   const orderedDates = arrayOfDates.sort((a: string, b: string) => {
     return Date.parse(b) - Date.parse(a);
   });
