f242c65835680bb4c6b50aa8e280ac2e4fc293a5
SianRichards <sian_richards@hotmail.co.uk>
2023-12-01 15:41:52 +0000

    create new types

--- trader-rota/src/types/index.ts
+++ trader-rota/src/types/index.ts
@@ -1,13 +1,17 @@
-export interface IDummyShiftData {
+export interface IShiftData {
   date: String;
   dow: String;
   shift: String;
   desk_1: String;
   desk_2: String;
   desk_3: String;
-};
+}
 
 export interface IShift {
   date: string;
   shiftType: string;
 }
+
+export type IRotaPlan = Array<IShiftData>;
+
+export type IData = Array<IRotaPlan>;
