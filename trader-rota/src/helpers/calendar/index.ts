5098ffbb6645f42e67680b66e786679ea0bc218e
SianRichards <sian_richards@hotmail.co.uk>
2023-12-04 16:27:11 +0000

    calendar helper gets day and date

--- trader-rota/src/helpers/calendar/index.ts
+++ trader-rota/src/helpers/calendar/index.ts
@@ -4,7 +4,11 @@ export const getAllDaysOfTheYear = (startDate: Date, endDate: Date) => {
   let currentDate = new Date(startDate);
 
   while (currentDate <= endDate) {
-    daysOfTheYear.push(new Date(currentDate).toLocaleDateString("en-UK"));
+    daysOfTheYear.push(
+      {
+        date: new Date(currentDate).toLocaleDateString("en-GB"),
+        day: new Date(currentDate).toLocaleString('en-GB', {  weekday: 'long' })
+      });
     currentDate.setDate(currentDate.getDate() + 1);
   }
   return daysOfTheYear;
