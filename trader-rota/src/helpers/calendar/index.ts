0153f9173bf64bd131eefa20a90970ea44c0860c
SianRichards <sian_richards@hotmail.co.uk>
2023-12-05 10:32:13 +0000

    get weeks function

--- trader-rota/src/helpers/calendar/index.ts
+++ trader-rota/src/helpers/calendar/index.ts
@@ -1,15 +1,43 @@
+import { dummyDates } from "../../dummyData";
+
 export const getAllDaysOfTheYear = (startDate: Date, endDate: Date) => {
   const daysOfTheYear = [];
 
   let currentDate = new Date(startDate);
 
   while (currentDate <= endDate) {
-    daysOfTheYear.push(
-      {
-        date: new Date(currentDate).toLocaleDateString("en-GB"),
-        day: new Date(currentDate).toLocaleString('en-GB', {  weekday: 'long' })
-      });
+    daysOfTheYear.push({
+      date: new Date(currentDate).toLocaleDateString("en-GB"),
+      day: new Date(currentDate).toLocaleString("en-GB", { weekday: "long" }),
+    });
     currentDate.setDate(currentDate.getDate() + 1);
   }
   return daysOfTheYear;
 };
+
+// get all dates between startDate and endDate
+// split the dates into weeks
+// for each week array replace each date with
+// a date and day object
+
+export const getWeeks = (dates: any) => {
+  const weeks: any = [];
+  let currentWeek: any = [];
+
+  dates.forEach((date: any) => {
+    currentWeek.push(date);
+
+    if (date.getDay() === 0) {
+      weeks.push(currentWeek);
+      currentWeek = [];
+    }
+  });
+  if (currentWeek.length > 0) {
+    weeks.push(currentWeek);
+  }
+  return weeks;
+};
+
+const arrayOfWeeks = getWeeks(dummyDates);
+
+console.log(arrayOfWeeks);
