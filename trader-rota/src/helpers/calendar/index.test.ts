d65dd5f7eabc06cea86a1c2b423b23b0dfec40fd
SianRichards <sian_richards@hotmail.co.uk>
2023-12-05 14:14:23 +0000

    calendar helper test

---
+++ trader-rota/src/helpers/calendar/index.test.ts
@@ -0,0 +1,9 @@
+// import { getWeeks } from ".";
+// import { dummyEndDate, dummyStartDate, dummyWeeks } from "../../dummyData";
+
+// describe("getWeeks", () => {
+//   it("should return an array containing arrays of dates", () => {
+//     const output = getWeeks(dummyStartDate, dummyEndDate);
+//     expect(output).toBe(dummyWeeks)
+//   });
+// });
